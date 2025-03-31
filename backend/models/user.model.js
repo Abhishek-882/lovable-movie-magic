2aa5925e740c1f5c266c55b6cf911f682542af0d
Abhishek-882 <varsha99633@gmail.com>
2025-03-31 20:17:50 +0530

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,44 +1,69 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcryptjs');
+import { Table, Column, Model, DataType, BeforeCreate, HasOne } from 'sequelize-typescript';
+import { Profile } from './profile.model';
+import bcrypt from 'bcryptjs';
 
-module.exports = (sequelize) => {
-  class User extends Model {
-    static associate(models) {
-      User.hasOne(models.Profile, { foreignKey: 'userId' });
-      User.hasMany(models.Booking, { foreignKey: 'userId' });
+@Table({
+  tableName: 'users',
+  timestamps: true,
+  paranoid: true
+})
+export class User extends Model {
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+    primaryKey: true
+  })
+  id: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
     }
+  })
+  email: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false
+  })
+  password: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false
+  })
+  name: string;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    defaultValue: false
+  })
+  isEmailVerified: boolean;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    defaultValue: false
+  })
+  isProfileComplete: boolean;
+
+  @HasOne(() => Profile)
+  profile: Profile;
+
+  @BeforeCreate
+  static async hashPassword(user: User) {
+    user.password = await bcrypt.hash(user.password, 10);
   }
 
-  User.init({
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: { isEmail: true }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      set(value) {
-        this.setDataValue('password', bcrypt.hashSync(value, 10));
-      }
-    },
-    name: DataTypes.STRING,
-    isEmailVerified: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    }
-  }, {
-    sequelize,
-    modelName: 'User',
-    tableName: 'users',
-    paranoid: true
-  });
+  async comparePassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+  }
 
-  return User;
-};
+  toJSON() {
+    const values = Object.assign({}, this.get());
+    delete values.password;
+    return values;
+  }
+}
