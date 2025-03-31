1e40d2faa059f14f43cf423f0c6f808eb085e345
Abhishek-882 <varsha99633@gmail.com>
2025-04-01 00:34:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,8 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src/client')
-    }
-  },
   build: {
-    outDir: './dist/client',
+    outDir: '../dist/client',
     emptyOutDir: true
   }
 });
